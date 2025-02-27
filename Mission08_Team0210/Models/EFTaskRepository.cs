6e8dc3315f12d2438c842dfb979142c1dd568a13
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2025-02-27 12:53:51 -0700

    added add remove update ef repository

--- Mission08_Team0210/Models/EFTaskRepository.cs
+++ Mission08_Team0210/Models/EFTaskRepository.cs
@@ -8,6 +8,23 @@
         {
             _context = temp;
         }
+
+        public void AddTask(Task task)
+        {
+            _context.Add(task);
+            _context.SaveChanges();
+        }
+        public void RemoveTask(Task task)
+        {
+            _context.Remove(task);
+            _context.SaveChanges();
+        }
+
+        public void UpdateTask(Task task)
+        {
+            _context.Update(task);
+            _context.SaveChanges();
+        }
         public List<Task> Tasks => _context.Tasks.ToList();
         public List<Category> Categories => _context.Categories.ToList();
     }
